d1a213ff47435e8b26f23d3b2e99c615f773dc11
willriley <wriley.email@gmail.com>
2016-05-16 17:51:30 -0400

    Craps game simulator

--- Textbook Projects/Craps/Craps.java
+++ Textbook Projects/Craps/Craps.java
@@ -1,4 +1,14 @@
-
+/*
+    Simulates a game of craps, a popular gambling game
+    that uses two dice. 
+    
+    Rules: Roll two dice. Check the sum of the two dice. 
+    If the sum is 2, 3, or 12 (called craps), you lose; 
+    if the sum is 7 or 11 (called natural), you win; 
+    anything else establishes a "point". Continue rolling 
+    until either a 7 or the same point value is rolled. 
+    If 7 is rolled, you lose. If the point is rolled, you win.
+*/
 public class Craps {
     public static void main(String[] args) {
         Roll dice = new Roll();
